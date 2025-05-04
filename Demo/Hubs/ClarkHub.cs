a01c6e5bdd71bedb94e8ac03883e5cbe85ace72a
Clark <clark2000s@gmail.com>
2025-05-04 23:47:40 +0800

    [新增] 專案

---
+++ Demo/Hubs/ClarkHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Demo.Hubs
+{
+    public class ClarkHub : Hub
+    {
+        public ClarkHub()
+        {
+        }
+
+        /// <summary>
+        /// 連線
+        /// </summary>
+        public async override Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+
+        /// <summary>
+        /// 連線中斷 
+        /// </summary>
+        /// <param name="exception">例外物件</param>
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
